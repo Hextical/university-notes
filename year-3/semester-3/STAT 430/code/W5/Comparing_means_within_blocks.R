027a8178328ca4bdc2a78055592cecbef050a44c
hexii@hexii <hexii@protonmail.com>
2021-06-10 22:23:34 -0400

    directory stuff + upload code

---
+++ year-3/semester-3/STAT 430/code/W5/Comparing_means_within_blocks.R
@@ -0,0 +1,115 @@
+## Comparing Multiple Means Within Blocks
+
+## Read-in the data
+setwd("/Users/nstevens/Dropbox/Teaching/STAT_430/Spring_2021/R Stuff/")
+gap <- read.csv("thegap.csv", header = TRUE)
+
+## Response by condition
+par(mfrow=c(1,2))
+boxplot(Y ~ Condition, data = gap, xlab = "Condition", ylab = "Purchase Total ($)", xaxt = "n", ylim = c(20,80))
+axis(side = 1, at = 1:3, labels = c("Promo 1", "Promo 2", "Promo 3"))
+mean.condition <- aggregate(x = gap$Y, by = list(Promo = gap$Condition), FUN = mean)
+mean.condition
+plot(x = 1:3, y = mean.condition$x, xlab = "Condition", ylab = "Avg. Purchase Total ($)", pch = 16, xaxt = "n", ylim = c(20,80))
+lines(x = 1:3, y = mean.condition$x)
+axis(side = 1, at = 1:3, labels = c("Promo 1", "Promo 2", "Promo 3"))
+
+## Response by block
+boxplot(Y ~ factor(Block, levels = c("M", "Tu", "W", "Th", "F"), labels = c("M", "Tu", "W", "Th", "F"),
+                   ordered = TRUE), data = gap, xlab = "Day of Week", ylab = "Purchase Total ($)", ylim = c(20,80))
+mean.block <- aggregate(x = gap$Y, by = list(Day = gap$Block), FUN = mean)
+mean.block <- mean.block[c(2,4,5,3,1),]
+mean.block
+plot(x = 1:5, y = mean.block$x, xlab = "Day of Week", ylab = "Avg. Purchase Total ($)", pch = 16, xaxt = "n", ylim = c(20,80))
+lines(x = 1:5, y = mean.block$x)
+axis(side = 1, at = 1:5, labels = c("M", "Tu", "W", "Th", "F"))
+
+
+## Fit the appropriate linear regression model and perform the relevant F-tests
+full <- lm(Y ~ Condition + Block, data = gap)
+summary(full)
+anova(full)
+
+## Partial F-tests
+red1 <- lm(Y ~ Block, data = gap)
+red2 <- lm(Y ~ Condition, data = gap)
+
+# Is there a difference in expected response between conditions?
+anova(red1, full)
+
+# Was blocking necessary?
+anova(red2, full)
+
+
+## Check model assumptions graphically
+par(mfrow=c(1,3))
+hist(full$residuals, xlab = "Residuals", main = "Histogram of Residuals", probability = TRUE)
+lines(x = seq(-10,10,0.01), y = dnorm(x = seq(-10,10,0.01), mean = mean(full$residuals), sd = sd(full$residuals)), col = "red")
+qqnorm(full$residuals)
+qqline(full$residuals, col = "red")
+plot(x = full$fitted.values, y = full$residuals, xlab = "Fitted Values", ylab = "Residuals", main = "Residuals vs. Fitted Values")
+abline(h = 0, col = "red")
+
+## Check model assumptions with hypothesis tests
+shapiro.test(x = full$residuals)
+group <- kronecker(1:15, rep(1,50))
+bartlett.test(x = full$residuals, g = group)
+
+
+## Now let's try to do this manually...
+N <- dim(gap)[1]
+m <- length(unique(gap$Condition))
+b <- length(unique(gap$Block))
+n <- 50
+
+# Condition-specific means:
+mean1 <- mean(gap$Y[gap$Condition == "V1"])
+mean2 <- mean(gap$Y[gap$Condition == "V2"])
+mean3 <- mean(gap$Y[gap$Condition == "V3"])
+
+# Block-specific means:
+meanM <- mean(gap$Y[gap$Block == "M"])
+meanTu <- mean(gap$Y[gap$Block == "Tu"])
+meanW <- mean(gap$Y[gap$Block == "W"])
+meanTh <- mean(gap$Y[gap$Block == "Th"])
+meanF <- mean(gap$Y[gap$Block == "F"])
+
+# Overall mean:
+meanOverall <- mean(gap$Y)
+
+SSC <- b*n*((mean1 - meanOverall)^2 + (mean2 - meanOverall)^2 + (mean3 - meanOverall)^2)
+SSB <- m*n*((meanM - meanOverall)^2 + (meanTu - meanOverall)^2 + (meanW - meanOverall)^2 + (meanTh - meanOverall)^2 + (meanF - meanOverall)^2)
+SSE <- sum((gap$Y[gap$Condition == "V1" & gap$Block == "M"] - mean1 - meanM + meanOverall)^2) + 
+       sum((gap$Y[gap$Condition == "V1" & gap$Block == "Tu"] - mean1 - meanTu + meanOverall)^2) + 
+       sum((gap$Y[gap$Condition == "V1" & gap$Block == "W"] - mean1 - meanW + meanOverall)^2) + 
+       sum((gap$Y[gap$Condition == "V1" & gap$Block == "Th"] - mean1 - meanTh + meanOverall)^2) + 
+       sum((gap$Y[gap$Condition == "V1" & gap$Block == "F"] - mean1 - meanF + meanOverall)^2) + 
+       sum((gap$Y[gap$Condition == "V2" & gap$Block == "M"] - mean2 - meanM + meanOverall)^2) + 
+       sum((gap$Y[gap$Condition == "V2" & gap$Block == "Tu"] - mean2 - meanTu + meanOverall)^2) + 
+       sum((gap$Y[gap$Condition == "V2" & gap$Block == "W"] - mean2 - meanW + meanOverall)^2) + 
+       sum((gap$Y[gap$Condition == "V2" & gap$Block == "Th"] - mean2 - meanTh + meanOverall)^2) + 
+       sum((gap$Y[gap$Condition == "V2" & gap$Block == "F"] - mean2 - meanF + meanOverall)^2) + 
+       sum((gap$Y[gap$Condition == "V3" & gap$Block == "M"] - mean3 - meanM + meanOverall)^2) + 
+       sum((gap$Y[gap$Condition == "V3" & gap$Block == "Tu"] - mean3 - meanTu + meanOverall)^2) + 
+       sum((gap$Y[gap$Condition == "V3" & gap$Block == "W"] - mean3 - meanW + meanOverall)^2) + 
+       sum((gap$Y[gap$Condition == "V3" & gap$Block == "Th"] -mean3 - meanTh + meanOverall)^2) + 
+       sum((gap$Y[gap$Condition == "V3" & gap$Block == "F"] -mean3 - meanF + meanOverall)^2)
+sum(c(SSC, SSB, SSE))
+SST <- var(gap$Y)*(N-1)
+SST
+df <- c(m-1, b-1, N-m-b+1)
+MS <- c(SSC, SSB, SSE) / df
+Tstat <- MS[1:2]/MS[3]
+pval <- pf(q = Tstat, df1 = df[1:2], df2 = rep(df[3], 2), lower.tail = FALSE)
+
+# Check that all of this work matched the anova output
+anova(full)
+c(SSC, SSB, SSE)
+MS
+Tstat
+pval
+
+
+
+
+
---
+++ year-3/semester-3/STAT 430/code/W5/Comparing_proportions_within_blocks.R
@@ -0,0 +1,53 @@
+## Comparing Multiple Proportions Within Blocks
+
+## Read-in the data
+setwd("/Users/nstevens/Dropbox/Teaching/STAT_430/Spring_2021/R Stuff/")
+enterprise <- read.csv("enterprise.csv", header = TRUE)
+
+## Response by condition
+mean.condition <- aggregate(x = enterprise$Y, by = list(Ad = enterprise$Condition), FUN = mean)
+mean.condition
+par(mfrow=c(1,2))
+plot(x = 1:3, y = mean.condition$x, xlab = "Condition", ylab = "Click Through Rate", pch = 16, xaxt = "n", ylim = c(0.05, 0.1))
+lines(x = 1:3, y = mean.condition$x)
+axis(side = 1, at = 1:3, labels = c("Ad1", "Ad2", "Ad3"))
+
+## Response by block
+mean.block <- aggregate(x = enterprise$Y, by = list(Region = enterprise$Block), FUN = mean)
+mean.block
+plot(x = 1:4, y = mean.block$x, xlab = "Region", ylab = "Click Through Rate", pch = 16, xaxt = "n", ylim = c(0.05, 0.1))
+lines(x = 1:4, y = mean.block$x)
+axis(side = 1, at = 1:4, labels = c("NE", "NW", "SE", "SW"))
+
+## Fit the appropriate logistic regression model and perform the relevant LR-tests
+full <- glm(Y ~ factor(Condition) + factor(Block), family = binomial(link = "logit"), data = enterprise)
+summary(full)
+
+## Likelihood Ratio Tests
+red1 <- glm(Y ~ factor(Block), family = binomial(link = "logit"), data = enterprise) 
+red2 <- glm(Y ~ factor(Condition), family = binomial(link = "logit"), data = enterprise) 
+
+
+# Is there a difference in expected response between conditions?
+anova(red1, full, test = "LRT")
+
+# Was blocking necessary?
+anova(red2, full, test = "LRT")
+
+
+## The LRTs manually...
+# Reduced 1 vs. Full
+t_C <- as.numeric(2*(logLik(full)-logLik(red1)))
+pval_C <- pchisq(q = t_C, df = 2, lower.tail = FALSE)
+t_C
+pval_C
+
+# Reduced 2 vs. Full
+t_B <- as.numeric(2*(logLik(full)-logLik(red2)))
+pval_B <- pchisq(q = t_B, df = 3, lower.tail = FALSE)
+t_B
+pval_B
+
+
+
+
